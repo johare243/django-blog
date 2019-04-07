434334f9ec0451b0319b8b8098545a2b6975df28
johare <johare243@gmail.com>
2019-04-07 13:00:17 -0400

    add section to index, css changes

--- jmo/settings.py
+++ jmo/settings.py
@@ -23,6 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
