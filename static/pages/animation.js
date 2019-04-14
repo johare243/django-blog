2164c3b4f757ba23f1a0e097e429bfe3b5024b2a
johare <johare243@gmail.com>
2019-04-13 21:23:59 -0400

    refactor css base index

--- static/pages/animation.js
+++ static/pages/animation.js
@@ -3,7 +3,20 @@ $(function() {
         $("#header-btn").on('click', function(e) {
             e.preventDefault();
             $('html, body').animate({
-                scrollTop: $("#main-categories").offset().top +40
+                scrollTop: $(".main-mid-container").offset().top -140
             }, 500);
         })
+        
+        window.onscroll = function() { stickyHeader() };
+        var navbar = document.getElementById("navbar");
+        console.log(navbar);
+        var sticky = navbar.offsetTop;
+        function stickyHeader() {
+                if (window.pageYOffset > sticky) {
+                        navbar.classList.add("sticky");
+                } else {
+                        navbar.classList.remove("sticky");
+                }
+        }
 });
+
